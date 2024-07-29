38c4bc8812f3333a8f06a70cbeaaed24169d937a
Ayan Kabden <a.kabden@documentolog.com>
2024-07-29 16:13:57 +0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
-import { isMobile, isMobileOnly, browserName, browserVersion } from 'react-device-detect';
+import { isMobile, isTablet, isMobileOnly, browserName, browserVersion, isSafari } from 'react-device-detect';
 
 function App() {
 
   return (
       <div>
         <p>isMobile: {isMobile ? 'Yes' : 'No'}</p>
+        <p>isTablet: {isTablet ? 'Yes' : 'No'}</p>
         <p>isMobileOnly: {isMobileOnly ? 'Yes' : 'No'}</p>
         <p>Browser: {browserName}</p>
         <p>Browser Version: {browserVersion}</p>
+        <p>is Safari: {isSafari? 'Yes' : 'No'}</p>
       </div>
   )
 }
